fbc42e53c737e306aa9b8ecdaca691b22f83164e
jintao.ljt <jintao.ljt@alibaba-inc.com>
2014-10-30 10:30:29 +0800

    Initial Commit

---
+++ SimpleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTable
+//
+//  Created by jintao on 14-10-30.
+//  Copyright (c) 2014年 jintao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTable
+//
+//  Created by jintao on 14-10-30.
+//  Copyright (c) 2014年 jintao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
